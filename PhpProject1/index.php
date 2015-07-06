7fd56428d29eed06973a525ad1de303ceddeeb27
Jairy <Jairy@jairy-a4a882465>
2015-07-06 15:16:47 -0400

    cambio

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,24 +11,7 @@ and open the template in the editor.
     </head>
     <body>
 <?php
-$edad=18;
-$genero= "F";
-$equipo= "V";
-if ($edad >= 18){
-	echo "<br>Bienvenido a la fiesta</br>";
-} else {
-	echo "<br>No puede pasar</br>";
-}
-if($genero == "F"){
-	echo"<br>Tiene Bebidas gratis</br>";
-} else{
-	echo"<br>Tiene que pagar</br>";
-}
-if($equipo == "V"){
-	echo"<br>Venga esos 5 hermano</br>";
-} else{
-	echo"<br>Fuera de aqui jajaja</br>";
-}
+
 ?>
     </body>
 </html>
